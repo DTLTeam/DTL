5de0ecebc04630530f65a16a54413b71dc1d4b04
ygslikewq1 <373972944@qq.com>
2018-01-30 16:57:59 +0800

    mine

--- IPOAsk/IPOAsk/UI/Mine/Controller/MineViewController.m
+++ IPOAsk/IPOAsk/UI/Mine/Controller/MineViewController.m
@@ -11,7 +11,7 @@
 #import "HeadViewTableViewCell.h"
 
 @interface MineViewController () <UITableViewDelegate,UITableViewDataSource>
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
@@ -25,22 +25,48 @@
     // Do any additional setup after loading the view.
     
     dataArr = @[@"我的钱包",@"申请成为答主",@"草稿箱",@"帮助中心",@"关于我们",@"设置"];
+    [self setupView];
     
-   
 }
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    
-    self.navigationController.tabBarController.tabBar.hidden = NO;
+    self.title = @"我的";
+    self.navigationController.navigationBar.translucent = YES;
     self.navigationController.navigationBarHidden = YES;
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.navigationController.tabBarController.tabBar.hidden = NO;
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     [[UINavigationBar appearance] setBarTintColor:[UIColor whiteColor]];
-    
     UINavigationBar *navigationBar = self.navigationController.navigationBar;
     [navigationBar setBackgroundImage:[UtilsCommon createImageWithColor:[UIColor clearColor]] forBarMetrics:UIBarMetricsDefault];
     [navigationBar setShadowImage:[UtilsCommon createImageWithColor:[UIColor clearColor]]];
     navigationBar.shadowImage = [UtilsCommon createImageWithColor:[UIColor clearColor]];
     
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+}
+
+- (void)setupView
+{
+    self.view.backgroundColor = [UIColor colorWithRed:237.0/255 green:237.0/255 blue:237.0/255 alpha:1];
+    
+    CGFloat height = 160 + 64 + dataArr.count * 50 + 9;
+    if (height + TABBAR_HEIGHT >= SCREEN_HEIGHT) {
+        height = SCREEN_HEIGHT;
+    }
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, height) style:UITableViewStylePlain];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    _tableView.bounces = NO;
+    [self.view addSubview:_tableView];
+    if (@available(iOS 11.0, *)) {
+        _tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
 }
 
 
@@ -77,9 +103,27 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.section == 0) {
-        return 170;
+        return 160 + 64;
     }
-    return 60;
+    return 50;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 10;
+    }
+    return 0;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    if (section == 0) {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 10)];
+        view.backgroundColor = [UIColor colorWithRed:237.0/255 green:237.0/255 blue:237.0/255 alpha:1];
+        return view;
+    }
+    return [[UIView alloc]init];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -126,6 +170,8 @@
             cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:identifier];
         }
         cell.textLabel.text = dataArr[indexPath.section - 1];
+        cell.textLabel.font = [UIFont systemFontOfSize:16];
+        cell.imageView.image = [UIImage imageNamed:dataArr[indexPath.section - 1]];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
         return cell;
     }
--- IPOAsk/IPOAsk/UI/Mine/View/HeadViewTableViewCell.m
+++ IPOAsk/IPOAsk/UI/Mine/View/HeadViewTableViewCell.m
@@ -50,27 +50,36 @@ typedef void(^ActionBlock)(NSInteger tag);
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         
+        UIImageView *bgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 160)];
+        bgView.image = [UIImage imageNamed:@"banner背景"];
+        [self addSubview:bgView];
+        
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, SCREEN_WIDTH, 44)];
+        label.textColor = [UIColor whiteColor];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.text = @"我的";
+        [self addSubview:label];
+        
         _headView = [[UIImageView alloc] init];
         _headView.layer.masksToBounds = YES;
         _headView.layer.cornerRadius = 30;
-        [_headView.layer setBorderWidth:1];
-        _headView.backgroundColor = [UIColor redColor];
-        [_headView.layer setBorderColor:[UIColor blackColor].CGColor];
+        _headView.backgroundColor = [UIColor whiteColor];
+        _headView.image = [UIImage imageNamed:@"默认头像"];
         [self addSubview:_headView];
         [_headView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.mas_equalTo(self).offset(25);
-            make.top.mas_equalTo(self).offset(40);
+            make.left.mas_equalTo(label).offset(25);
+            make.top.mas_equalTo(label.mas_bottom);
             make.size.mas_equalTo(CGSizeMake(60, 60));
         }];
         
         _nameLabel = [[UILabel alloc] init];
         _nameLabel.font = [UIFont systemFontOfSize:15];
         _nameLabel.text = @"骑着妞妞打呼噜";
-        _nameLabel.textColor = [UIColor blackColor];
+        _nameLabel.textColor = [UIColor whiteColor];
         _nameLabel.textAlignment = NSTextAlignmentLeft;
         [self addSubview:_nameLabel];
         [_nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.top.mas_equalTo(self).offset(50);
+            make.top.mas_equalTo(label.mas_bottom).offset(10);
             make.left.mas_equalTo(_headView.mas_left).offset(80);
             make.size.mas_equalTo(CGSizeMake(120, 20));
         }];
@@ -78,7 +87,7 @@ typedef void(^ActionBlock)(NSInteger tag);
         _phoneLabel = [[UILabel alloc] init];
         _phoneLabel.font = [UIFont systemFontOfSize:13];
         _phoneLabel.text = @"123*******11";
-        _phoneLabel.textColor = [UIColor blackColor];
+        _phoneLabel.textColor = [UIColor whiteColor];
         _phoneLabel.textAlignment = NSTextAlignmentLeft;
         [self addSubview:_phoneLabel];
         [_phoneLabel mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -90,56 +99,51 @@ typedef void(^ActionBlock)(NSInteger tag);
         _askButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _askButton.titleLabel.font = [UIFont systemFontOfSize:14];
         _askButton.titleLabel.numberOfLines = 0;
-        [_askButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [_askButton setAttributedTitle:[self getAttributedStringWithString:@"1\n我的提问" lineSpace:5] forState:UIControlStateNormal];
         [_askButton addTarget:self action:@selector(askAction:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_askButton];
         [_askButton mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(self).offset(0);
-            make.bottom.mas_equalTo(self).offset(0);
+            make.bottom.mas_equalTo(self).offset(-5);
             make.size.mas_equalTo(CGSizeMake(SCREEN_WIDTH/4, 50));
         }];
        
         _answerButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _answerButton.titleLabel.font = [UIFont systemFontOfSize:14];
         _answerButton.titleLabel.numberOfLines = 0;
-        [_answerButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [_answerButton setAttributedTitle:[self getAttributedStringWithString:@"1\n我的回答" lineSpace:5] forState:UIControlStateNormal];
         [_answerButton addTarget:self action:@selector(answerAction:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_answerButton];
         [_answerButton mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(_askButton).offset(SCREEN_WIDTH/4);
-            make.bottom.mas_equalTo(self).offset(0);
+            make.bottom.mas_equalTo(self).offset(-5);
             make.size.mas_equalTo(CGSizeMake(SCREEN_WIDTH/4, 50));
         }];
         
         _followButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _followButton.titleLabel.font = [UIFont systemFontOfSize:14];
         _followButton.titleLabel.numberOfLines = 0;
-        [_followButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [_followButton setAttributedTitle:[self getAttributedStringWithString:@"1\n我的关注" lineSpace:5] forState:UIControlStateNormal];
         [_followButton addTarget:self action:@selector(followAction:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_followButton];
         [_followButton mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(_answerButton).offset(SCREEN_WIDTH/4);
-            make.bottom.mas_equalTo(self).offset(0);
+            make.bottom.mas_equalTo(self).offset(-5);
             make.size.mas_equalTo(CGSizeMake(SCREEN_WIDTH/4, 50));
         }];
 
         _likeButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _likeButton.titleLabel.font = [UIFont systemFontOfSize:14];
         _likeButton.titleLabel.numberOfLines = 0;
-        [_likeButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
         [_likeButton setAttributedTitle:[self getAttributedStringWithString:@"1\n我的成就" lineSpace:5] forState:UIControlStateNormal];
         [_likeButton addTarget:self action:@selector(likeAction:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:_likeButton];
         [_likeButton mas_makeConstraints:^(MASConstraintMaker *make) {
             make.left.mas_equalTo(_followButton).offset(SCREEN_WIDTH/4);
-            make.bottom.mas_equalTo(self).offset(0);
+            make.bottom.mas_equalTo(self).offset(-5);
             make.size.mas_equalTo(CGSizeMake(SCREEN_WIDTH/4, 50));
         }];
-        
-        
+    
         _actionBlock = block;
         
     }
