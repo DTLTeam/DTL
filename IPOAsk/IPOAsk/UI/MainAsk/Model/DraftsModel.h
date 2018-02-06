aaf6c51de67e724b5da896cc2e4062ced16de63b
zjb <371453500@qq.com>
2018-02-06 09:36:36 +0800

    添加model

--- IPOAsk/IPOAsk/UI/MainAsk/Model/DraftsModel.h
+++ IPOAsk/IPOAsk/UI/MainAsk/Model/DraftsModel.h
@@ -13,6 +13,7 @@
 @property (strong, nonatomic) NSString *title;    //标题
 @property (strong, nonatomic) NSString *content;  //内容
 @property (assign, nonatomic) NSInteger Type;     //草稿类型 个人回答 个人提问 企业提问
-@property(nonatomic,strong)NSString *Id;
+@property(nonatomic,strong)   NSString *Id;
+@property(nonatomic,strong)   NSString *anonymous; //匿名
 
 @end
